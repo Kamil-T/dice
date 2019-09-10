feb1d19677617661bd8e682c5549287719de5fb4
Kamil T <trzaskomakamil@gmail.com>
2019-09-10 21:54:44 +0200

    Updates

---
+++ precache-manifest.00aa4f583ff3654d35e3e39e5bc5fc34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21308bb92f51f1313fd0017879a6602d",
+    "url": "/dice/index.html"
+  },
+  {
+    "revision": "e30758511b714a53bde8",
+    "url": "/dice/static/css/main.c9699233.chunk.css"
+  },
+  {
+    "revision": "54c1ef0fabc528478dd7",
+    "url": "/dice/static/js/2.5951504a.chunk.js"
+  },
+  {
+    "revision": "e30758511b714a53bde8",
+    "url": "/dice/static/js/main.1a375ffb.chunk.js"
+  },
+  {
+    "revision": "26380a6e54ff6ccfcb3d",
+    "url": "/dice/static/js/runtime~main.5a0d654d.js"
+  }
+]);
